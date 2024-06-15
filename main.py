fe8c778b2c3a009f28f0808a6c124dd7fa23bc11
Davis Tang <ngkaokis@gmail.com>
2024-06-16 01:28:04 +0800

    refactor: Decouple client script and server worker

--- main.py
+++ main.py
@@ -1,10 +1,17 @@
-from server.worker import process_csv_file_task
 import os
+from celery import Celery
 
 if __name__ == "__main__":
+    app = Celery(
+        "worker",
+        broker=os.environ.get("BROKER_URL", ""),
+    )
     for filename in os.listdir("data"):
         f = os.path.join("data", filename)
         if os.path.isfile(f):
-            process_csv_file_task.delay(
-                file_path=f,
+            app.send_task(
+                "server.worker.celery.process_csv_file_task",
+                kwargs={
+                    "file_path": f,
+                },
             )
